ecf880c65fad36b3ae554fbedcd5f0b7a98d4b5f
Sindi Andriana <70883212+SaderekSatia@users.noreply.github.com>
2023-09-03 01:25:17 +0700

    Update 2-arch-install.sh

--- 2-arch-install.sh
+++ 2-arch-install.sh
@@ -14,10 +14,10 @@ echo "127.0.1.1 anarki.localdomain anarki" >> /etc/hosts
 printf "\e[1;32mMasukan Kata Sandi ROOT\e[0m\n"
 passwd
 
-pacman -S linux-zen linux-zen-headers linux-zen-docs linux-firmware sof-firmware alsa-firmware virt-firmware acpid acpi acpi_call
-pacman -S btrfs-progs grub grub-btrfs efibootmgr efivar efitools mtools dosfstools gvfs gvfs-smb os-prober ntfs-3g
+# pacman -S acpid acpi acpi_call
+pacman -S e2fsprogs btrfs-progs dosfstools exfatprogs f2fs-tools ntfs-3g mtools gvfs gvfs-smb efivar efitools efibootmgr grub grub-btrfs os-prober
 pacman -S alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-audio pipewire-media-session pipewire-jack noise-suppression-for-voice
-pacman -S networkmanager network-manager-applet avahi dialog wpa_supplicant nfs-utils inetutils dnsutils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld nss-mdns bluez bluez-utils aria2 openssh shadowsocks-libev simple-obfs android-tools net-tools
+pacman -S networkmanager network-manager-applet avahi dialog wpa_supplicant nfs-utils inetutils dnsutils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld nss-mdns bluez bluez-utils aria2 openssh shadowsocks simple-obfs android-tools net-tools
 pacman -S libvirt virt-manager edk2-ovmf bridge-utils make fakeroot
 pacman -S bash-completion git gvim neovim neovide
 pacman -S xf86-video-amdgpu
@@ -28,15 +28,15 @@ grub-mkconfig -o /boot/grub/grub.cfg
 
 systemctl enable NetworkManager
 systemctl enable bluetooth
-#systemctl enable cups.service
-#systemctl enable sshd
-#systemctl enable avahi-daemon
-#systemctl enable tlp
-#systemctl enable reflector.timer
-#systemctl enable fstrim.timer
+# systemctl enable acpid
+# systemctl enable cups.service
+# systemctl enable sshd
+# systemctl enable avahi-daemon
+# systemctl enable tlp
+# systemctl enable reflector.timer
+# systemctl enable fstrim.timer
 systemctl enable libvirtd
 systemctl enable firewalld
-systemctl enable acpid
 
 useradd -m sadereksatia
 usermod -aG wheel sadereksatia
